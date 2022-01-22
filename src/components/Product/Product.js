3910cf0573f4d77c4db2006ddc3f70d292b58376
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-01-22 23:41:17 +0100

    Add render and size choice

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,71 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+//import Products from '../Products/Products';
+import { useState } from 'react'
 
-const Product = props => {
+
+const Product = ({ title, basePrice, colors, sizes, name }) => {
+  const [currentColor, setCurrentColor] = useState(colors[0])
+  const [currentSize, setCurrentSize] = useState(sizes[0])
+
+   //console.log('currentSize:', currentSize)
+   //console.log('sizes.name:' , currentSize.size)
+  
+
+  const colorClass = (color) => {
+    return styles[
+      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
+    ];
+  }
+   
+    
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt= {title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
+              {sizes.map((sizes) => (
+                <li key={sizes.name}>
+                  <button
+                    type='button'
+                    className={clsx(sizes.name === currentSize && styles.active)}
+                    onClick={() => setCurrentSize(sizes.name)}
+                  >
+                    {sizes.name}
+                  </button>
+                </li>
+              ))}
+              {/* <li><button type="button" className={styles.active}>S</button></li>
               <li><button type="button">M</button></li>
               <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              <li><button type="button">XL</button></li> */}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map((color) => (
+                <li key={color}>
+                  <button
+                    type='button'
+                    className={clsx(colorClass(color), color === currentColor && styles.active)}
+                    onClick={() => setCurrentColor(color)}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,32 @@
 import { useState } from 'react';
 import productsData from '../../data/products';
 import Product from '../Product/Product';
+import PropTypes from 'prop-types';
 
 const Products = () => {
   const [products]  = useState(productsData);
-
+    
   return (
     <section>
-      {productsData.map(product => <Product key={product.id} name={product.name} title={product.title} colors={product.colors} size={product.size} baseProce={product.basePrice} />)}
+      {productsData.map(product => <Product key={product.id} name={product.name} title={product.title} colors={product.colors} sizes={product.sizes} basePrice={product.basePrice} />)}
     </section>
   );
-};
+  /*
+return (
+    <section>
+      {products.map((product, index) => {
+        return <Product {...products[index]} key={product.id} />;
+      })}
+    </section>
+  );
+*/
+ };
+
+
+Products.protoTypes = {
+productsData: PropTypes.array.isRequired
+
+}
+
 
 export default Products;
